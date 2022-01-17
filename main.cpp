c2d0c3467c1c29834c76c86bb6aa836c321156f8
xa1ax <ksergei74@gmail.com>
2022-01-18 01:35:38 +0500

    Added substration

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter two integer number" << endl;
+    cin >> a >> b;
+    int d;
+    d = a + b;
+    count << "a + b =" << d << endl;
+    d = a - b;
+    count << "a + b =" << d << endl;
+}
 
